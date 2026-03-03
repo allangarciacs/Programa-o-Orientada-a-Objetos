484519838902fffa94dce033d4852da3ac7271e1
Allan Storch Garcia <allan.storch21@gmail.com>
2026-03-03 11:01:47 -0300

    Add files via upload

---
+++ aulas/aula 02/Exercicio10.java
@@ -0,0 +1,29 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+		double n, soma = 0, media = 0;
+		int i = 0;
+		
+		while(true) {
+			System.out.println("Digite um valor: ");
+			n = teclado.nextDouble();
+			soma += n; //somatorio = somatorio + n
+			if(n<=0) {
+				break;
+			}
+			i++;
+		}
+		media = soma / i;
+		System.out.println("Media: "+media);
+
+		teclado.close();
+		
+	}
+
+}
